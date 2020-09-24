9ee57ffb160137f0959878bbb8063e3dadade2e4
Nikhil GPS <Nikhil GPS@LAPTOP-KODMKQBD>
2020-09-24 17:56:48 +0530

    All entities added, Development starts here

--- src/test/java/com/capgemini/pecunia/PassbookControllerTest.java
+++ src/test/java/com/capgemini/pecunia/PassbookControllerTest.java
@@ -20,36 +20,5 @@ import com.capgemini.pecunia.service.PassbookService;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PassbookControllerTest {
-   
-                                                 
-    @MockBean                           
-    private PassbookService passbookService;
-                                               
-    private List<Transaction> transactionList;       
-                                            
- 
-    
-    @Test
-    public void testShouldGetAllTransactions() throws Exception{	
-      this.transactionList = new ArrayList<Transaction>();                    
-      this.transactionList.add(new Transaction("TR001", "Debit", 3000.0,LocalDate.of(2020, 9, 2),  "100000000001", null));  
-      this.transactionList.add(new Transaction("TR002", "Credit", 4000.0,LocalDate.of(2020, 9, 10), "100000000001", null));
-    	when(passbookService.accountSummary("100000000001",LocalDate.of(2020, 9, 2),LocalDate.of(2020, 9, 10))).thenReturn(transactionList);
-    	assertEquals(2,passbookService.accountSummary("100000000001",LocalDate.of(2020, 9, 2),LocalDate.of(2020, 9, 10)).size());
-    }
-    
-    @Test
-    public void testAccountValidation() throws Exception{
-    	when(passbookService.accountValidation("100000000001")).thenReturn(true);
-    	assertEquals(true, passbookService.accountValidation("100000000001"));
-    }
-    
-    @Test
-    public void testUpdatePassbook() throws Exception{
-        this.transactionList = new ArrayList<Transaction>();                    
-        this.transactionList.add(new Transaction("TR001", "Debit", 3000.0,LocalDate.of(2020, 9, 2),  "100000000001", null));  
-        this.transactionList.add(new Transaction("TR002", "Credit", 4000.0,LocalDate.of(2020, 9, 10), "100000000001", null));
-    	when(passbookService.updatePassbook("100000000001")).thenReturn(transactionList);
-    	assertEquals(2, passbookService.updatePassbook("100000000001").size());
-    }
+
 }
--- src/test/java/com/capgemini/pecunia/TransactionManagementTests.java
+++ src/test/java/com/capgemini/pecunia/TransactionManagementTests.java
@@ -25,59 +25,5 @@ import com.capgemini.pecunia.service.TransactionService;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TransactionManagementTests {
-	@Autowired
-	private TransactionService transactionService;
-	
-	@MockBean
-	private TransactionDAO transactionDao;
-	
-	@MockBean
-	private AccountsDAO accountsDao;
-	
-	private Account account;
-	 private Transaction transaction1;
-	 private Cheque cheque;
-	
-	@BeforeEach                           
-     void setUp() {                                                                                      
-		// account1=new Account("HDFC100111","Amar", "111", "Savings",
-		//		"Active", 42000.00, LocalDate.of(2020, 8, 30));
-		 transaction1=new Transaction("1000101", "slip", 2000.00, LocalDate.of(2020, 9, 24), "HDFC100111", null);
-    }
-	
-	@Test
-	public void creditUsingSlipTest() {
-		account=new Account("HDFC100111","Amar", "111", "Savings",
-				"Active", 40000.00, LocalDate.of(2020, 8, 30));
-		when(accountsDao.getOne("HDFC100111")).thenReturn(new Account("HDFC100111","Amar", "111", "Savings",
-				"Active", 40000.00, LocalDate.of(2020, 8, 30)));
-		assertEquals(account,transactionService.creditUsingSlip("HDFC100111", 2000.00));
-	}
-	
-	@Test
-	public void creditUsingChequeTest() {
-		cheque=new Cheque("HDFCCH001", 1000101, "HDFC100111", "Amar",
-				"HDFC", "HDFC00661", LocalDate.of(2020, 9, 23) , null);
-		when(accountsDao.getOne("HDFC100111")).thenReturn(new Account("HDFC100111","Amar", "111", "Savings",
-				"Active", 40000.00, LocalDate.of(2020, 8, 30)));
-		assertEquals("Bounce",transactionService.creditUsingCheque(cheque, 44000.00));
-	}
-	
-	@Test
-	public void debitUsingSlipWithInsufficientBalanceTest() {
-		account=new Account("HDFC100111","Amar", "111", "Savings",
-				"Active", 42000.00, LocalDate.of(2020, 8, 30));
-		when(accountsDao.getOne("HDFC100111")).thenReturn(new Account("HDFC100111","Amar", "111", "Savings",
-				"Active", 40000.00, LocalDate.of(2020, 8, 30)));
-		assertEquals(false,transactionService.debitUsingSlip("HDFC100111", 41000.00));
-	}
-	@Test
-	public void debitUsingSlipWithSufficientBalanceTest() {
-		account=new Account("HDFC100111","Amar", "111", "Savings",
-				"Active", 42000.00, LocalDate.of(2020, 8, 30));
-		when(accountsDao.getOne("HDFC100111")).thenReturn(new Account("HDFC100111","Amar", "111", "Savings",
-				"Active", 40000.00, LocalDate.of(2020, 8, 30)));
-		assertEquals(true,transactionService.debitUsingSlip("HDFC100111", 1000.00));
-	}
 	
 }
